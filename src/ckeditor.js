5457d1245934969be438c412e44a94f552cb45a6
delphinelods <delphine.lods@symbol-it.fr>
2022-03-31 15:58:26 +0200

    New Ckeditor PO builder

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,21 +6,16 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 
@@ -31,21 +26,16 @@ Editor.builtinPlugins = [
 	Autoformat,
 	BlockQuote,
 	Bold,
-	CKFinderUploadAdapter,
 	Essentials,
 	Heading,
 	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
 	Indent,
+	IndentBlock,
 	Italic,
 	Link,
 	List,
 	MediaEmbed,
 	Paragraph,
-	PasteFromOffice,
 	SourceEditing,
 	TextTransformation
 ];
@@ -61,28 +51,19 @@ Editor.defaultConfig = {
 			'bold',
 			'italic',
 			'|',
+			'indent',
+			'outdent',
+			'|',
 			'bulletedList',
 			'numberedList',
 			'blockQuote',
 			'|',
-			'outdent',
-			'indent',
-			'|',
 			'link',
 			'mediaEmbed',
-			'|',
 			'sourceEditing'
 		]
 	},
-	language: 'fr',
-	image: {
-		toolbar: [
-			'imageTextAlternative',
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side'
-		]
-	}
+	language: 'fr'
 };
 
 export default Editor;
